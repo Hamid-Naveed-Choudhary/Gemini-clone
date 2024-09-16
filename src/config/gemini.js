ab678f2dc16bfae69b93a72b0d195ee7f7fe8596
Hamid Naveed <hamidnaveedch@gmail.com>
2024-09-16 18:19:08 +0500

    Update files for build creation

--- src/config/gemini.js
+++ src/config/gemini.js
@@ -1,51 +1,51 @@
 import {
-    GoogleGenerativeAI,
-    HarmCategory,
-    HarmBlockThreshold,
+  GoogleGenerativeAI,
+  HarmCategory,
+  HarmBlockThreshold,
 } from "@google/generative-ai"
 
 const MODEL_NAME = "gemini-1.5-flash"
-const API_KEY = "AIzaSyDkT0FHJC6K9kwM-AmU8H1oCQ-Iv1WFNb8"
+const API_KEY = import.meta.env.VITE_GOOGLE_API_KEY
 
 async function runChat(prompt) {
-    const genAI = new GoogleGenerativeAI(API_KEY)
-    const model = genAI.getGenerativeModel({ model: MODEL_NAME })
-    const generationConfig = {
-        temperature: 1,
-        topP: 0.95,
-        topK: 64,
-        maxOutputTokens: 8192,
-    }
+  const genAI = new GoogleGenerativeAI(API_KEY)
+  const model = genAI.getGenerativeModel({ model: MODEL_NAME })
+  const generationConfig = {
+    temperature: 1,
+    topP: 0.95,
+    topK: 64,
+    maxOutputTokens: 8192,
+  }
 
-    const safetySettings = [
-        {
-            category: HarmCategory.HARM_CATEGORY_HARASSMENT,
-            threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE
-        },
-        {
-            category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
-            threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-        },
-        {
-            category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
-            threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-        },
-        {
-            category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
-            threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
-        },
-    ];
+  const safetySettings = [
+    {
+      category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    },
+    {
+      category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    },
+    {
+      category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    },
+    {
+      category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+      threshold: HarmBlockThreshold.BLOCK_MEDIUM_AND_ABOVE,
+    },
+  ]
 
-    const chat = model.startChat({
-        generationConfig,
-        safetySettings,
-        history: [],
-    });
+  const chat = model.startChat({
+    generationConfig,
+    safetySettings,
+    history: [],
+  })
 
-    const result = await chat.sendMessage(prompt);
-    const response = result.response;
-    console.log(response.text());
-    return response.text();
+  const result = await chat.sendMessage(prompt)
+  const response = result.response
+  console.log(response.text())
+  return response.text()
 }
 
-export default runChat;
+export default runChat
